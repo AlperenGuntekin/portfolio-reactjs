33f527bc19081a840f7f2ecdf9a33883fab65386
AlperenGuntekin <alprngntkn@hotmail.com>
2022-07-14 20:24:58 +0300

    version1.1

---
+++ README.md
@@ -0,0 +1,30 @@
+<div align="center">
+  <h1>🌵<br>React Portfolio</h1>
+  <h3>Portfolio One-Pager made with ReactJs</h3>
+</div>
+
+<div align="center"><img src="https://user-images.githubusercontent.com/29565530/144163917-196b3e87-90e2-4615-b1c7-6a905533f34b.gif" /></div>
+<div align="center">
+  <p>Home background effect made with React-Particles</p>
+</div>
+
+<br>
+
+
+## 🛠️ Packages & APIs
+
+- [React](https://reactjs.org/)
+- [graphql](https://graphql.org/) 
+- [apollo-boost](https://www.apollographql.com/docs/react/get-started/)
+- [particles.js](https://github.com/VincentGarreau/particles.js/)
+- [react-reveal](https://github.com/rnosov/react-reveal)
+- [react-is-visible](https://github.com/lessp/react-is-visible)
+
+
+## 🚧 Current Planned Developments:
+
+- Add gif previews in a carousel of my previous web projects
+- Additional styling overhaul using css variables instead of manual colours
+- Convert site to a gatsby static site for faster load times & security
+
+
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Contact.css'
+import Section from '../section/Section'
+import { Bounce } from 'react-reveal'
+import ContactForm from '../contactForm/ContactForm'
+import linkedin from '../../images/social/linkedin.png'
+import github from '../../images/social/github.png'
+import instagram from '../../images/social/instagram.png'
+
+
+const Contact = () => {
+  return (
+    <Section title="Contact">
+      <ContactForm />
+      <Bounce cascade>
+        <div className="links">
+          <a
+            href="https://github.com/AlperenGuntekin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="Github Logo" width="40px" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alperenguntekin/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedin} alt="Linkedin Logo" width="40px" />
+          </a>
+
+          <a
+            href="https://www.instagram.com/alperenguntekin/?hl=tr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={instagram} alt="Instagram Logo" width="40px" />
+          </a>
+        </div>
+      </Bounce>
+    </Section>
+  )
+}
+
+export default Contact
